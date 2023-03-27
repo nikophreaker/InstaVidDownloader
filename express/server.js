0fc100eb918fdb97e1b0d806acb1b5c29bf9c087
nikophreaker <nikx449@gmail.com>
2023-03-27 15:29:08 +0700

    update

--- express/server.js
+++ express/server.js
@@ -73,7 +73,7 @@ exports.handler = serverless(app);
 function fetch(url, reject, resolve) {
     // execute phantom-script.js file via PhantomJS
     const childArgs = [path.join(__dirname, "phantom-script.js")];
-    const phantom = childProcess.execFile("./phantomjs.exe", childArgs, {
+    const phantom = childProcess.execFile("../../phantomjs.exe", childArgs, {
         env: {
             URL: url
         },
